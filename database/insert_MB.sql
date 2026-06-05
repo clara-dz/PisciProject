d2afce43ff5e9b09f0caaf627897dd34c9ee2202
Joao Victor Mendes de Siqueira <joaosiqueira.2005@alunos.utfpr.edu.br>
2026-06-05 16:30:55 -0300

    Aí tem as coisas do banco não ta tudo certinho, mas depois concerto o que ficou errado

---
+++ database/insert_MB.sql
@@ -0,0 +1,35 @@
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('ROG STRIX Z790-E', 'ASUS', 'LGA1700', 'Z790', 'ATX', '305x244', 4, 'DDR5', 7800, 192, '5.0', '1x16, 2x4', 5, '5.0', 4, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('MAG B650 TOMAHAWK', 'MSI', 'AM5', 'B650', 'ATX', '305x244', 4, 'DDR5', 6600, 128, '4.0', '1x16, 1x4', 3, '4.0', 6, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('B450M STEEL LEGEND', 'ASRock', 'AM4', 'B450', 'Micro-ATX', '244x244', 4, 'DDR4', 3533, 128, '3.0', '1x16, 1x4', 2, '3.0', 6, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('ROG STRIX B550-I GAMING', 'ASUS', 'AM4', 'B550', 'Mini-ITX', '170x170', 2, 'DDR4', 5100, 64, '4.0', '1x16', 2, '4.0', 4, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('GIGABYTE Z690 AORUS ELITE', 'GIGABYTE', 'LGA1700', 'Z690', 'ATX', '305x244', 4, 'DDR4', 5333, 128, '5.0', '1x16, 2x4', 4, '4.0', 6, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('TUF GAMING B760M-PLUS', 'ASUS', 'LGA1700', 'B760', 'Micro-ATX', '244x244', 4, 'DDR5', 7200, 192, '5.0', '1x16, 1x4', 2, '4.0', 4, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('ASROCK X670E TAICHI', 'ASRock', 'AM5', 'X670E', 'E-ATX', '305x267', 4, 'DDR5', 6600, 192, '5.0', '2x16', 4, '5.0', 8, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('GIGABYTE A520M S2H', 'GIGABYTE', 'AM4', 'A520', 'Micro-ATX', '244x205', 2, 'DDR4', 5100, 64, '3.0', '1x16', 1, '3.0', 4, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('EVGA Z790 DARK K|NGP|N', 'EVGA', 'LGA1700', 'Z790', 'E-ATX', '305x277', 2, 'DDR5', 8000, 96, '5.0', '2x16', 3, '4.0', 8, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('MSI H610M-G', 'MSI', 'LGA1700', 'H610', 'Micro-ATX', '244x211', 2, 'DDR4', 3200, 64, '4.0', '1x16', 1, '3.0', 4, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('ROG CROSSHAIR X670E HERO', 'ASUS', 'AM5', 'X670E', 'ATX', '305x244', 4, 'DDR5', 6400, 128, '5.0', '2x16', 5, '5.0', 6, NULL);
+
+INSERT INTO MotherBoard (Name, Manufacturer, MB_Socket, Chipset, form_factor, dimensions_mm, Slots_Ram, Ram_type, Ram_max_vel, Ram_max_cap, Pcie_Version, Pcie_x16_slots, m2_slots, M2_pcie_version, Sata_ports, Image_Data) 
+VALUES ('BIOSTAR B550MX/E PRO', 'BIOSTAR', 'AM4', 'B550', 'Micro-ATX', '244x244', 4, 'DDR4', 4933, 128, '4.0', '1x16, 1x4', 2, '4.0', 4, NULL);
---
+++ database/insert_cpu.sql
@@ -0,0 +1,16 @@
+#todas as imagens devem ser adicionadas posteriormente atraves de um select + botão direito + selecionar a imagem manualmente, após isso clique em aply e finish.
+insert into CPU values (0, "Intel Core i3-10100", "Intel", "1200", 65 , true, null);
+insert into CPU values (1, "AMD Athlon 3000G", "AMD", "AM4", 35 , true, null);
+insert into CPU values (2, "Intel Core i3-12100F", "Intel", "1700", 58 , false, null);
+insert into CPU values (3, "Intel Core i5-13400F", "Intel", "1700", 65 , false, null);
+insert into CPU values (4, "Intel Core i9-14900K", "Intel", "1700", 125 , true, null);
+insert into CPU values (5, "Intel Core i9-14900KS", "Intel", "1700", 150 , true, null);
+insert into CPU values (6, "AMD Ryzen 5 5600", "AMD", "AM4", 65 , false, null);
+insert into CPU values (7, "AMD Ryzen 5 3600", "AMD", "AM4", 65 , false, null);
+insert into CPU values (8, "AMD Ryzen 5 7600", "AMD", "AM5", 65 , false, null);
+insert into CPU values (9, "AMD Ryzen 7 7800X3D", "AMD", "AM5", 120 , false, null);
+insert into CPU values (10, "AMD Ryzen 7 5700G", "AMD", "AM4", 65 , true, null);
+insert into CPU values (11, "Intel Core i7-14700KF", "Intel", "1700", 125 , false, null);
+insert into CPU values (12, "AMD Ryzen 9 5950X", "AMD", "AM4", 105 , false, null);
+select * from CPU;
+#para ver a imagem clique com o botão direito na caixinha blob e vizualize ela em outra aba(mude para imagem).
